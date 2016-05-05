76eed4db1b5d4a0860658b75cf04358cfe0f7d4c
Vincent Narbot <vnarbot@takeflight.io>
2016-05-05 19:09:32 -0400

    [ADD] Book belong to an user / Can't edit if you are not the owner

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,17 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @book = current_user.books.build
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+
+    if @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -28,15 +38,7 @@ class BooksController < ApplicationController
     redirect_to root_path
   end
 
-  def create
-    @book = Book.new(book_params)
 
-    if @book.save
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
 
   private
     def book_params
